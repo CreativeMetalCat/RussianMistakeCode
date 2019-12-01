497751dd0dceccafe381ce8542cc70f3252d1310
MetalCat <36876492+CreativeMetalCat@users.noreply.github.com>
2019-12-01 19:08:06 +1000

    properly fixed #10

--- Private/HideAndSeek/HiddingPlaceBase.cpp
+++ Private/HideAndSeek/HiddingPlaceBase.cpp
@@ -4,6 +4,7 @@
 #include "HiddingPlaceBase.h"
 #include "GameFramework/Character.h"
 #include "GameFramework/CharacterMovementComponent.h"
+#include "Engine.h"
 #include "Public/PlayerInteractions.h"
 
 void AHiddingPlaceBase::OnInteraction_Implementation(AActor* interactor, UPrimitiveComponent* interactedComponent)
@@ -21,10 +22,12 @@ void AHiddingPlaceBase::OnInteraction_Implementation(AActor* interactor, UPrimit
 			IPlayerInteractions::Execute_ForceCrouch(interactor);
 			FCameraLimitsInfo info = IPlayerInteractions::Execute_GetCameraLimitations(interactor);
 			info.bSupposedToLimit = true;
-			info.MaxRoll = 180 + GetActorRotation().Roll;
+			info.MaxRoll = 180 +  GetActorRotation().Roll;
 			info.MinRoll = -180 + GetActorRotation().Roll;
-			info.MaxYaw = -180 - GetActorRotation().Yaw;
-			info.MinYaw = 0 - GetActorRotation().Yaw;
+			info.MaxYaw = (GetActorRotation().Yaw > 0 || GetActorRotation().Yaw < -90 ? 0 : -180) + (GetActorRotation().Yaw > 0 || GetActorRotation().Yaw < -90 ? 1 : -1)* GetActorRotation().Yaw;
+			info.MinYaw = (GetActorRotation().Yaw > 0 || GetActorRotation().Yaw < -90 ? -180 : 0) + (GetActorRotation().Yaw > 0 || GetActorRotation().Yaw < -90 ? 1 : -1)* GetActorRotation().Yaw;
+
+			if (GEngine) { GEngine->AddOnScreenDebugMessage(-1, 2.0f, FColor::Red, FString::FromInt(info.MaxYaw).Append(" Min:").Append(FString::FromInt(info.MinYaw))); }
 			IPlayerInteractions::Execute_SetCameraLimitations(interactor, info);
 			IPlayerInteractions::Execute_SetIsHidding(interactor, true);
 
